6b1f8430527863c31aca11638a25915aba42f3f9
yongjun321 <362727065@qq.com>
2017-05-18 11:04:15 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+﻿// JavaScript Document
+
+$(function(){
+   $("#sure").click(function(){
+	   $(".sub_header").fadeIn();
+	   return false;
+   })
+    $("#jisuan").click(function(){
+	   $(".container").fadeIn();
+	   return false;
+   })
+})
